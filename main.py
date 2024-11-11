ee0fcbe0600e3023a78b2401b9ba3d24daecd15c
qqqx <you@example.com>
2024-11-11 10:30:36 +0300

    Код оптимизирован

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+def first():
+    print("1. ***")
+    print("   ***")
+    print("   ***")
+
+def second():
+    print("2.  * ")
+    print("   ***")
+    print("    * ")
+
+def third():
+    print("3. 999")
+    print("   888")
+    print("   777")
+
+def four():
+    print("4. 111")
+    print("   222")
+    print("   333")
+
 print("1. ***")
 print("   ***")
 print("   ***")
@@ -12,22 +32,13 @@ print("   222")
 print("   333")
 a = int(input("Введите цифру от 1 до 4: "))
 
-zzzz
 
 
 if a == 1:
-    print("1. ***")
-    print("   ***")
-    print("   ***")
+    print(first())
 if a == 2:
-    print("2.  * ")
-    print("   ***")
-    print("    * ")
+    print(second())
 if a == 3:
-    print("3. 999")
-    print("   888")
-    print("   777")
+    print(third())
 if a == 4:
-    print("4. 111")
-    print("   222")
-    print("   333")
+    print(four())
