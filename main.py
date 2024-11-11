619ca2bdac45a8464bb7e896de0db10a228c20bf
qqqx <you@example.com>
2024-11-11 10:19:00 +0300

    Добавлен файл main.py с меню из 3 пунктов

---
+++ main.py
@@ -0,0 +1,23 @@
+print("1. ***")
+print("   ***")
+print("   ***")
+print("2.  * ")
+print("   ***")
+print("    * ")
+print("3. 999")
+print("   888")
+print("   777")
+a = int(input("Введите цифру от 1 до 3: "))
+
+if a == 1:
+    print("1. ***")
+    print("   ***")
+    print("   ***")
+if a == 2:
+    print("2.  * ")
+    print("   ***")
+    print("    * ")
+if a == 3:
+    print("3. 999")
+    print("   888")
+    print("   777")
