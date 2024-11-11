8b3d79f0ac369b1bae28f06fa1f9f6eb7fe78ef7
qqqx <you@example.com>
2024-11-11 10:38:39 +0300

    Добавлена библиотека my_library.py

---
+++ my_library.py
@@ -0,0 +1,19 @@
+def first():
+    print("1. ***")
+    print("   ***")
+    print("   ***")
+
+def second():
+    print("2.  * ")
+    print("   ***")
+    print("    * ")
+
+def third():
+    print("3. 999")
+    print("   888")
+    print("   777")
+
+def four():
+    print("4. 111")
+    print("   222")
+    print("   333")
